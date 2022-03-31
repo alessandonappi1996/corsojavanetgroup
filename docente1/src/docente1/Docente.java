73b23ce4c72691e3abc57cdf11ecac8ea1decdc6
alessandronappi1996 <alex.jk1996@gmail.com>
2022-03-31 09:28:14 +0200

    primo commit

---
+++ docente1/src/docente1/Docente.java
@@ -0,0 +1,30 @@
+package docente1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Docente {
+	public static void main(String args[]) {}
+	String nome="luca";
+	String cognome="espo";
+	int matricola=1;
+	String nome1="ale";
+	String cognome1="nap";
+	int matricola1=2;
+	String nome2="gio";
+	String cognome2="marco";
+	int matricola2=3;
+
+
+
+	List<Docente> pippo = new ArrayList<>();
+
+	         pippo.add(nome,cognome,matricola,nome1,cognome1,matricola1,nome2,cognome2,matricola2);
+	        
+
+	
+}
+
+
+
+    
